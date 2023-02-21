3b47bc21ba480fb33e800a9f770c728daaec88db
usha-12 <usha12ahirwar@gmail.com>
2023-02-22 00:29:37 +0530

    Added: rerieve all the Entries from the DB

--- src/main/java/com/brideglabz/addressbookdb/AddressBook.java
+++ src/main/java/com/brideglabz/addressbookdb/AddressBook.java
@@ -1,2 +1,55 @@
-package com.brideglabz.addressbookdb;public class AddressBook {
+package com.brideglabz.addressbookdb;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    Connection connection;
+
+    private Connection getConnection() {
+        String URL_JD = "jdbc:mysql://127.0.0.1:3306/addressbookservice?useSSL=false";
+        String USER_NAME = "root";
+        String PASSWORD = "Usha@123";
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Drivers loaded!!");
+            connection = DriverManager.getConnection(URL_JD,USER_NAME,PASSWORD);
+            System.out.println("connection Established!!");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+
+    public List<Contacts> retrieveData() {
+        ResultSet resultSet = null;
+        List<Contacts> addressBookList = new ArrayList<Contacts>();
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            String sql = "select * from AddressBook";
+            resultSet = statement.executeQuery(sql);
+            int count = 0;
+            while (resultSet.next()) {
+                Contacts contactInfo = new Contacts();
+                contactInfo.setFirstName(resultSet.getString("first_name"));
+                contactInfo.setLastName(resultSet.getString("last_name"));
+                contactInfo.setAddress(resultSet.getString("address"));
+                contactInfo.setCity(resultSet.getString("city"));
+                contactInfo.setState(resultSet.getString("state"));
+                contactInfo.setZip(resultSet.getInt("zip"));
+                contactInfo.setPhoneNumber(resultSet.getString("phone_number"));
+                contactInfo.setEmailId(resultSet.getString("email"));
+                contactInfo.setBookName(resultSet.getString("type"));
+
+                addressBookList.add(contactInfo);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return addressBookList;
+
+    }
 }
